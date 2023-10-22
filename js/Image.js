c7b2c336dbe78949dbb2f38ebef8dfdc5d504662
PaulofmRodrigues <paulofilipe_100pt@hotmail.com>
2023-10-22 16:14:50 +0100

    new commit

--- js/Image.js
+++ js/Image.js
@@ -1,6 +1,7 @@
 //imports
 import Vector3 from "./Vector3.js";
 import Ray from "./Ray.js";
+import Color3 from "./Color3.js";
 
 export default class Image {
     constructor(gl, W, H, imageInfo) {
@@ -14,13 +15,19 @@ export default class Image {
         this.blue = imageInfo[4];
 
         this.vertices = [];
+        this.origin = new Vector3(0.0, 0.0, 30.0);
+        this.s = this.h / this.vertices.length;
+        this.ray;
     }
 
     create() {
+        this.createVertices(this.horizontal, this.vertical);
+
+        const color = this.traceRay(this.ray, 0);
+
+        console.log(color);
         this.gl.clearColor(this.red, this.green, this.blue, 1);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT);
-
-        this.createVertices(this.horizontal, this.vertical);
 
         const vertexBuffer = this.gl.createBuffer();
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
@@ -28,24 +35,33 @@ export default class Image {
     }
 
     createVertices(horizontal, vertical) {
-        const origin = new Vector3(0.0, 0.0, 30.0);
-        const s = this.h / this.vertices.length;
-
-        for (let i = 0; i < this.vertices.length; i++) {
-            for (let j = 0; j < this.horizontal.length; j++) {
-                const px = (i + 0.5) * s - this.w / 2.0;
-                const py = -(j + 0.5) * s + this.h / 2.0;
+        for (let i = 0; i < vertical.length; i++) {
+            for (let j = 0; j < horizontal.length; j++) {
+                const px = (i + 0.5) * this.s - this.w / 2.0;
+                const py = -(j + 0.5) * this.s + this.h / 2.0;
                 const pz = 0.0;
 
                 const direction = new Vector3(px, py, - 30.0);
                 direction.normalize();
-                
-                const ray = new Ray(origin, direction);
+
+                this.ray = new Ray(origin, direction);
             }
         }
     }
 
     traceRay(ray, rec) {
-        
+        hit = new Hit();
+
+        hit.found = false; // inicialização; também pode ser realizada no construtor da classe Hit
+        hit.tmin = 1.0 * 10 ** 12; // usem um valor muito elevado. Por exemplo, hit.tmin = 1.0E12;
+
+        sceneObjects.forEach(object => {
+            // ciclo para percorrer todos os objectos da cena
+            object.intersect(ray, hit);
+        });
+        if (hit.found)
+            return hit.material.color; // se houver intersecção, retorna a cor do material constituinte do objecto intersectado mais próximo da origem do raio
+        else
+            return image.backgroundColor; // caso contrário, retorna a cor de fundo
     }
 }
